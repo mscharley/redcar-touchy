a51903a9303eee86c1d87d6cbf7bbb58e964433f
Matthew Scharley <matt@scharley.me>
2010-07-24 22:00:57 +1000

    Initial version of the touchy module. The project module should work with the code here when it's complete.

--- lib/touchy.rb
+++ lib/touchy.rb
@@ -1,6 +1,15 @@
 
 module Redcar
   class Touchy
-    
+    def self.project_context_menus
+      Menu::Builder.build do
+        item "Touch File", TouchFileCommand
+      end
+    end
+
+    class TouchFileCommand < Redcar::Command
+      def execute
+        Application::Dialog.message_box("Oooh, touchy, touchy.")
+      end
   end
 end
