4c6186b5c2df1095fe94b715bfee3f4e0737522f
Matthew Scharley <matt@scharley.me>
2010-07-28 22:03:17 +1000

    Added sanity checking, fixing a bug with right click on no node.

--- touchy.rb
+++ touchy.rb
@@ -3,7 +3,7 @@ module Redcar
   class Touchy
     def self.project_context_menus(tree, node, controller)
       Menu::Builder.build do
-        if node.file?
+        if not node.nil? and node.file?
           separator
           item("Touch File") { node.adapter.touch(node.path) }
         end
